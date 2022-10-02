9cb0b2f8b2671d53a499791ef408a9fe933b0eda
paul <423357+toothbrush@users.noreply.github.com>
2022-10-02 18:03:55 -0400

    Initial Commit

---
+++ SwiftMenu/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftMenu
+//
+//  Created by paul on 2/10/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
