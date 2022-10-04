f2c05ba0565b5811a2f8d0a5c9c12d293320bb47
paul <423357+toothbrush@users.noreply.github.com>
2022-10-04 00:30:52 -0400

    PDApplication subclasses NSApplication: avoid mousey things.

---
+++ SwiftMenu/Subclasses/PDApplication.swift
@@ -0,0 +1,42 @@
+//
+//  PDApplication.swift
+//  SwiftMenu
+//
+//  Created by paul on 4/10/2022.
+//
+
+import Cocoa
+
+@objc(PDApplication)
+class PDApplication: NSApplication {
+
+    // let's avoid mousey things happening m'kay
+    override func sendEvent(_ event: NSEvent) {
+        let ignores : [NSEvent.EventType] = [
+            .leftMouseDown,
+            .leftMouseUp,
+            .rightMouseDown,
+            .rightMouseUp,
+            .mouseMoved,
+            .leftMouseDragged,
+            .rightMouseDragged,
+            .mouseEntered,
+            .mouseExited,
+            .scrollWheel,
+            .otherMouseDown,
+            .otherMouseUp,
+            .otherMouseDragged,
+            .gesture,
+            .magnify,
+            .swipe,
+            .rotate,
+            .beginGesture,
+            .endGesture,
+            .smartMagnify,
+        ]
+        if ignores.contains(event.type) {
+            return
+        }
+        super.sendEvent(event)
+    }
+}
