bce6bc4a68c396ba8b059effe69bd7e906bb19cf
paul <423357+toothbrush@users.noreply.github.com>
2022-10-04 23:44:02 -0400

    PDTextField: Lotsa gymnastics to keep text-selection-length 0 always!

--- SwiftMenu/Subclasses/PDTextField.swift
+++ SwiftMenu/Subclasses/PDTextField.swift
@@ -37,4 +37,47 @@ class PDTextField: NSTextField {
     override func validateProposedFirstResponder(_ responder: NSResponder, for event: NSEvent?) -> Bool {
         return true
     }
+
+    // This was far from obvious to me, but these helped:
+    // - https://stackoverflow.com/questions/25705232/track-selection-range-change-for-nstextfield-cocoa
+    override func becomeFirstResponder() -> Bool {
+        let res = super.becomeFirstResponder()
+
+        if let ed = self.currentEditor() {
+            ed.delegate = self
+        }
+
+        return res
+    }
 }
+
+extension PDTextField: NSTextViewDelegate {
+    // can we be smart about disallowing range-select?
+    // note we needed to do magic to sort out the (ever-changing!) currentEditor's delegate!
+    func textView(_ textView: NSTextView,
+                  willChangeSelectionFromCharacterRanges oldSelectedCharRanges: [NSValue],
+                  toCharacterRanges newSelectedCharRanges: [NSValue]) -> [NSValue] {
+        // - https://stackoverflow.com/questions/27040924/nsrange-from-swift-range
+        guard newSelectedCharRanges.count == 1 else {
+            print("omg wtf, how can you be selecting _multiple_ ranges?")
+            return [NSValue(range: NSMakeRange(0, 0))]
+        }
+
+        guard let newRange = newSelectedCharRanges.first as? NSRange else {
+            print("hm, newSelectedCharRanges is not an NSRange??")
+            return [NSValue(range: NSMakeRange(0, 0))]
+        }
+        guard let oldRange = oldSelectedCharRanges.first as? NSRange else {
+            print("hm, oldSelectedCharRanges is not an NSRange??")
+            return [NSValue(range: NSMakeRange(0, 0))]
+        }
+
+        if newRange.length == 0 {
+            // all good, just moving cursor, let the user do whatever
+            return newSelectedCharRanges
+        } else { // something else - just leave old range's position i guess 🤷
+            return [NSValue(range: NSMakeRange(oldRange.location, 0))]
+        }
+    }
+}
+
