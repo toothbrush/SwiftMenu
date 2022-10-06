12ad7d83a42e934bc341ca04bcf28e5c61ba6836
paul <423357+toothbrush@users.noreply.github.com>
2022-10-05 20:52:55 -0400

    Tricked-out PDWindow with PDSquareView

--- SwiftMenu/Subclasses/PDWindow.swift
+++ SwiftMenu/Subclasses/PDWindow.swift
@@ -11,6 +11,29 @@ class PDWindow: NSWindow {
 
     let myFieldEditor = PDTextView()
 
+    override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
+        super.init(contentRect: contentRect,
+                   styleMask: .borderless,
+                   backing: .buffered,
+                   defer: false)
+        contentView!.wantsLayer = true
+        self.alphaValue = 1.0
+        self.isOpaque = false
+        assert(self.canBecomeKey)
+        assert(self.canBecomeMain)
+    }
+    
+    override public var canBecomeKey: Bool {
+        get {
+            return true
+        }
+    }
+    override public var canBecomeMain: Bool {
+        get {
+            return true
+        }
+    }
+
     override func validateProposedFirstResponder(_ responder: NSResponder, for event: NSEvent?) -> Bool {
         return false
     }
