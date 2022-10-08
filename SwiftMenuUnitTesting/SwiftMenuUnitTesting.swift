acecc06f56c011dcbc9da6f75f5047fabf71b8a8
paul <423357+toothbrush@users.noreply.github.com>
2022-10-07 23:12:29 -0400

    Add some more specific matching tests.

--- SwiftMenuUnitTesting/SwiftMenuUnitTesting.swift
+++ SwiftMenuUnitTesting/SwiftMenuUnitTesting.swift
@@ -35,6 +35,34 @@ class SwiftMenuUnitTesting: XCTestCase {
         XCTAssert(PasswordList.filteredEntriesList(filter: "foo", entries: passwordList).contains("foozoo.com"))
     }
 
+    func testPrefixPreferred() {
+        // we're hoping that given two options, "zoo" and "foozoo", searching for "zoo" will initially match as if you searched for "^zoo", because it's.. closer?  Maybe this will be
+        XCTAssertEqual(
+            PasswordList.filteredEntriesList(filter: "zoo", entries: passwordList).first!,
+            "zoo.com.au")
+    }
+
+    func testSubstringMatches() {
+        XCTAssertGreaterThan(
+            PasswordList.filteredEntriesList(filter: "app fr", entries: passwordList).count,
+            0)
+        XCTAssertEqual(
+            PasswordList.filteredEntriesList(filter: "app   fr", entries: passwordList).first,
+            "appleid/frank@example.com")
+        XCTAssertGreaterThan(
+            PasswordList.filteredEntriesList(filter: "app   fr exa", entries: passwordList).count,
+            0)
+        XCTAssertEqual(
+            PasswordList.filteredEntriesList(filter: "app   fr exa", entries: passwordList).first,
+            "appleid/frank@example.com")
+    }
+
+    func testSubstringOnlyMatchesInOrder() {
+        XCTAssertEqual(
+            PasswordList.filteredEntriesList(filter: "app   exa    frank", entries: passwordList).count,
+            0) // this shouldn't match apple/frank@example, since "example" comes after "frank"
+    }
+
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         measure {
