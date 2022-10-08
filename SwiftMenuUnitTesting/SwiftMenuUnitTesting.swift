0d1c61b3afbb25324cb29ee6b02f920e69cb5cf8
paul <423357+toothbrush@users.noreply.github.com>
2022-10-07 22:06:44 -0400

    Set up a small testing framework for matching/filtering

---
+++ SwiftMenuUnitTesting/SwiftMenuUnitTesting.swift
@@ -0,0 +1,37 @@
+//
+//  SwiftMenuUnitTesting.swift
+//  SwiftMenuUnitTesting
+//
+//  Created by paul on 7/10/2022.
+//
+
+import XCTest
+
+class SwiftMenuUnitTesting: XCTestCase {
+
+    var passwordList: [String]!
+
+    override func setUpWithError() throws {
+        passwordList = [
+            "foozoo.com",
+            "zoo.com.au",
+        ]
+    }
+
+    override func tearDownWithError() throws {
+        passwordList = nil
+    }
+
+    func testEmptyFilterWorks() throws {
+        XCTAssert(passwordList.count > 0)
+        XCTAssert(PasswordList.filteredEntriesList(filter: "  ", entries: passwordList) == passwordList)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            let _ = try! PasswordList.prettyPasswordsList()
+        }
+    }
+
+}
