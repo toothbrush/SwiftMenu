9f4a70df1b0dcd47a825889e16a19e815fce4172
paul <423357+toothbrush@users.noreply.github.com>
2024-11-30 21:14:53 +1100

    Ditch KeyboardShortcuts; try the old-fashioned way.  Making progress.

---
+++ SwiftMenu/HotKeySolution.swift
@@ -0,0 +1,88 @@
+//
+//  HotKeySolution.swift
+//  SwiftMenu
+//
+//  Created by paul on 30/11/2024.
+//
+
+import Carbon
+import Cocoa
+
+class HotkeySolution {
+    /// See https://stackoverflow.com/questions/28281653/how-to-listen-to-global-hotkeys-with-swift-in-a-macos-app, a Swift translation of venerable global hotkey stuff that works.
+    static func getCarbonFlagsFromCocoaFlags(cocoaFlags: NSEvent.ModifierFlags) -> UInt32 {
+        let flags = cocoaFlags.rawValue
+        var newFlags = 0
+
+        if (flags & NSEvent.ModifierFlags.control.rawValue) > 0 {
+            newFlags |= controlKey
+        }
+
+        if (flags & NSEvent.ModifierFlags.command.rawValue) > 0 {
+            newFlags |= cmdKey
+        }
+
+        if (flags & NSEvent.ModifierFlags.shift.rawValue) > 0 {
+            newFlags |= shiftKey
+        }
+
+        if (flags & NSEvent.ModifierFlags.option.rawValue) > 0 {
+            newFlags |= optionKey
+        }
+
+        if (flags & NSEvent.ModifierFlags.capsLock.rawValue) > 0 {
+            newFlags |= alphaLock
+        }
+
+        return UInt32(newFlags)
+    }
+
+    static func registerCmdR() {
+        var hotKeyRef: EventHotKeyRef?
+        let modifierFlags: UInt32 =
+            getCarbonFlagsFromCocoaFlags(cocoaFlags: NSEvent.ModifierFlags.command)
+
+        let keyCode = kVK_ANSI_R
+        var gMyHotKeyID = EventHotKeyID()
+
+        gMyHotKeyID.id = UInt32(keyCode)
+
+        // Not sure what "swat" vs "htk1" do.
+        gMyHotKeyID.signature = OSType("swat".fourCharCodeValue)
+        // gMyHotKeyID.signature = OSType("htk1".fourCharCodeValue)
+
+        var eventType = EventTypeSpec()
+        eventType.eventClass = OSType(kEventClassKeyboard)
+        eventType.eventKind = OSType(kEventHotKeyReleased)
+
+        // Install handler.
+        // Want to know what this signature should be?  And the handler lambda??  Well fuck you.  Or look here, https://github.com/davedelong/DDHotKey/blob/e0481f648e0bc7e55d183622b00510b6721152d8/DDHotKeyCenter.m#L19C64-L19C70, because there's no official doc to be found.
+        InstallEventHandler(GetApplicationEventTarget(), {
+            _, theEvent, _ -> OSStatus in
+            var hkCom = EventHotKeyID()
+
+            GetEventParameter(theEvent,
+                              EventParamName(kEventParamDirectObject),
+                              EventParamType(typeEventHotKeyID),
+                              nil,
+                              MemoryLayout<EventHotKeyID>.size,
+                              nil,
+                              &hkCom)
+
+            NSLog("Command + R Released!")
+//            NSLog(hkCom)
+
+            return noErr
+            /// Check that hkCom in indeed your hotkey ID and handle it.
+        }, 1, &eventType, nil, nil)
+
+        // Register hotkey.
+        let status = RegisterEventHotKey(UInt32(keyCode),
+                                         modifierFlags,
+                                         gMyHotKeyID,
+                                         GetApplicationEventTarget(),
+                                         0,
+                                         &hotKeyRef)
+        assert(status == noErr)
+    }
+}
